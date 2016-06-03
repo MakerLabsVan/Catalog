8e20b89760abb261c2f71e38361561e5b4e013c2
Sunny <sunny.hl.leung@gmail.com>
2016-06-02 23:32:54 -0700

    Building map objects with google sheets
    In progress

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
-var express = require('express');
-var app = express();
+const PORT=3000;
 
-
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/', function(request, response){
+    response.sendfile('index.html');
 });
-
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
--- d3demo.js
+++ d3demo.js
@@ -1,25 +1,26 @@
-//
-
+//item will be place in the map as shapes
 var rectangleData = [
   {"rx":0,"ry":0,"height":200, "width":225,"color":"blue"},
   {"rx":200, "ry":200,"height":50,"width":50,"color":"red"},
   {"rx":1400, "ry":1100,"height":50,"width":50,"color":"green"}];
 
+//Container for map and items
 var svgContainer =d3.select("body").append("svg")
-  .attr("width", 1400)
-  .attr("height",1400);
+  .attr("width", 700)
+  .attr("height",500)
+  .style("border","1px solid black");
 
+//add rectangles to svg
 var rectangles = svgContainer.selectAll("rect")
   .data(rectangleData)
   .enter()
   .append("rect")
-  .on("mouseover",function(){
+  .on("mouseover",function(){//Need to fix translation
     d3.select(this).transition().attr("transform","scale(1.2)")
   })
   .on("mouseout",function(){
     d3.select(this).transition().attr("transform","scale(1)")
   });
-
 
 var rectangleAttributes = rectangles
   .attr("x", function (d) { return d.rx; })
@@ -29,16 +30,16 @@ var rectangleAttributes = rectangles
   .style("fill", function(d) { return d.color; });
 
 
-
-  var level1= d3.xml("level1.svg", "image/svg+xml", function(error, xml) {
+//Add map and add to svgContainer
+var level1= d3.xml("level1.svg", "image/svg+xml", function(error, xml) {
     // Insert the SVG image as a DOM node
   svgContainer.node().appendChild(document.importNode(xml.documentElement, true));
-
   // Acess and manipulate the iamge
   var svgImage = svgContainer.select('svg');
-
   svgImage
-      .attr('width', 1400)
-      .attr('height', 1400); });
+      .attr('width', 700)
+      .attr('height', 500)
+      .classed("svg-content-responsive",true);
+  });
 
 d3.select("body").append("p").text("compiled");
