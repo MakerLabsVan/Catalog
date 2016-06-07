d7b0cbd1645078fb97b58faa57470a98dfd7a6ae
Andrew Song <Andrew Song>
2016-06-06 18:37:15 -0700

    Create live search feature to find query in JSON object in their respective categories

--- app/components/index/index.js
+++ app/components/index/index.js
@@ -1,36 +1,13 @@
 var myApp = angular.module("myApp", []);
 
-myApp.controller("MainCtrl", ["$scope", function ($scope) {
+myApp.controller("MainCtrl", ["$scope", '$http', function ($scope, $http) {
     $scope.categories = {
         "studio": "Studios",
         "tools": "Tools",
         "cons": "Consumables",
         "mats": "Materials"
     };
-}]);
 
-myApp.controller("UserCtrl", ["$scope", function ($scope) {
-    // namespace user details
-    $scope.user = {};
-    // details is arbitrary naming scheme
-    $scope.user.details = {
-        "Andrew": {
-            "username": "Andrew Song",
-            "id": "89101112"
-        },
-        "Sunny": {
-            "username": "Sunny L",
-            "id": "123456"
-        },
-        "Brian": {
-            "username": "Brian N",
-            "id": "654321"
-        }
-    };
-
-}]);
-
-myApp.controller("DataCtrl", ['$scope', '$http', function ($scope, $http) {
     $http({
         method: 'GET',
         url: '//localhost:3000/getData'
@@ -41,5 +18,8 @@ myApp.controller("DataCtrl", ['$scope', '$http', function ($scope, $http) {
         })
         .error(function (data, status, header, config) {
             // something went wrong
+            alert("Something went wrong! Please call for help!");
         });
+
+    $scope.queryTerm = '';
 }]);
