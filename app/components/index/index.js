e6247bb6271cbeca15ede2c7d53d115b59fc6d7b
Andrew Song <Andrew Song>
2016-06-06 18:52:27 -0700

    Add code to display the number of objects in each category

--- app/components/index/index.js
+++ app/components/index/index.js
@@ -15,6 +15,22 @@ myApp.controller("MainCtrl", ["$scope", '$http', function ($scope, $http) {
         .success(function (data, status, header, config) {
             // success data
             $scope.data = data;
+
+            var studtemp = 0;
+            var tooltemp = 0;
+            var mattemp = 0;
+            var contemp = 0;
+            for (var i = 0; i < data.length; i++) {
+                if (data[i][1] === "Studio") studtemp++;
+                if (data[i][1] === "Tool") tooltemp++;
+                if (data[i][1] === "Consumable") contemp++;
+                if (data[i][1] === "Material") mattemp++;
+            }
+            $scope.studioSize = studtemp;
+            $scope.toolSize = tooltemp;
+            $scope.matSize = mattemp;
+            $scope.conSize = contemp;
+
         })
         .error(function (data, status, header, config) {
             // something went wrong
