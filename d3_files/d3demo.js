ac8431309f08bbe4747af46dde6d6f9e3373aa4e
Sunny <sunny.hl.leung@gmail.com>
2016-06-13 19:00:51 -0700

    Marker

--- d3_files/d3demo.js
+++ d3_files/d3demo.js
@@ -9,6 +9,11 @@ var container = "#map-well";
 
 //initialize map
 function init(width, height){
+
+  var aspectL1 = 1.25385;//W=1364.490,H=1088.246
+  var aspectL2 = 0.85035; // W=925.374,H=1088.238
+  if (width/height > aspectL1){var scaleL1=height/1088.246; }
+  else {var scaleL1=width/1364.490;}
   //Container for map and map information
   var svgContainer=d3.select(container).append("svg")
     .attr("width", width)
@@ -16,7 +21,7 @@ function init(width, height){
     .attr("id", "svgMapContainer")
     .style("border","1px solid black")
     .on("click", function(){
-      marker.attr("cx",d3.mouse(this)[0]+"px").attr("cy",d3.mouse(this)[1]+"px");
+      marker.attr("x",d3.mouse(this)[0]+"px").attr("y",d3.mouse(this)[1]+"px");
       marker.style("visibility", "visible");
     });
 
@@ -24,14 +29,15 @@ function init(width, height){
       var level1= d3.xml("../d3_files/level1.svg", "image/svg+xml", function(xml) {
         svgContainer.node().appendChild(document.importNode(xml.documentElement, true));
         svgContainer.select('svg')
-            .attr("cx",0)
-            .attr("cy",0)
+            .attr("x",0)
+            .attr("y",0)
             .attr('height', height)
             .attr('width', width)
             .attr("preserveAspectRatio", "xMinYMin meet")
-            .attr("id","map");
+            .attr("id","level1");
       });
 
+/*
     var tooltip = svgContainer
       .append("text")
       .attr("x", 50)
@@ -40,28 +46,27 @@ function init(width, height){
       .attr("font-size", "20px")
       .attr("fill", "red")
       .attr("visibility","hidden");
-
+*/
     var marker = svgContainer
-      .append("circle")
-      .attr("r", 10)
-      .attr("fill", "blue")
-      .style("visibility","hidden")
-      .attr("id","marker");
+    .append("svg:image")
+    .attr("xlink:href", "../d3_files/marker.svg")
+    .style("visibility","hidden")
+    .attr("id","marker")
+    .attr("x",50)
+    .attr("y",50)
+    .attr("width",50)
+    .attr("height",50);
 
-    var aspect1 = 1.25385;
-    var aspect2 = 0.85035;
-    if (width/height > aspect1){var scaleFactor=height/1088.246}
-    else {var scaleFactor=width/1364.490;}
 
       //Rectangles represent studio spaces
     var rectangles = svgContainer.selectAll("rect")
       .data(rectangleData)
       .enter()
       .append("rect")
-      .attr("x", function (d) { return (d.rx/10)*scaleFactor+"in"; })//Assumes that dimensions are in ft, 500 is the pixel height of map
-      .attr("y", function (d) { return (d.ry/10)*scaleFactor+"in"; })
-      .attr("height", function (d) { return (d.height/10)*scaleFactor+"in"; })
-      .attr("width", function (d) { return (d.width/10)*scaleFactor+"in"; })
+      .attr("x", function (d) { return (d.rx/10)*scaleL1+"in"; })//Assumes that dimensions are in ft, 500 is the pixel height of map
+      .attr("y", function (d) { return (d.ry/10)*scaleL1+"in"; })
+      .attr("height", function (d) { return (d.height/10)*scaleL1+"in"; })
+      .attr("width", function (d) { return (d.width/10)*scaleL1+"in"; })
       .attr('id',function (d) { return d.id; })
       .attr("floor", function(d) { return d.floor; })
       .style("color","blue")
