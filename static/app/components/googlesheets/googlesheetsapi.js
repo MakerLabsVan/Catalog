3401443da8f806c2ac134f94a8e2499a7d0163ca
Andrew Song <Andrew Song>
2016-06-21 15:29:06 -0700

    Remove unneccessary variable in deleting entries

--- static/app/components/googlesheets/googlesheetsapi.js
+++ static/app/components/googlesheets/googlesheetsapi.js
@@ -128,7 +128,7 @@ var listMajors = function (auth, callback) {
 
 var sheetWrite = function (auth, message, row) {
 
-    var nextRow = 'A' + String(row + 6) + ':N';
+    var nextRow = 'A' + String(row + 2) + ':N';
 
     var sheets = google.sheets('v4');
     sheets.spreadsheets.values.update({
@@ -147,15 +147,14 @@ var sheetWrite = function (auth, message, row) {
 };
 
 var deleteEntry = function (auth, index) {
-    var row = index + 5;
     var body = {
         "requests": [
             {
                 "deleteDimension": {
                     "range": {
                         "dimension": "ROWS",
-                        "startIndex": row,
-                        "endIndex": row + 1
+                        "startIndex": index + 1,
+                        "endIndex": index + 2
                     }
                 }
             }
