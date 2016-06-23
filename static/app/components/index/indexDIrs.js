738b20a044b7a2365f5206cb7125c5ed40f3a07a
Andrew Song <Andrew Song>
2016-06-23 13:13:54 -0700

    Create but not call directive to display category data using a tag attribute as the filter

--- static/app/components/index/indexDIrs.js
+++ static/app/components/index/indexDIrs.js
@@ -4,5 +4,15 @@ angular.module("myApp").directive("indexSearchRes", function () {
         templateUrl: 'templates/indexSearchResTmpl.html',
         link: function (scope, elem, attrs) {
         }
-    }
-})
+    };
+});
+
+// angular.module("myApp").directive("categoryRes", function () {
+//     return {
+//         templateUrl: 'templates/categoryResTmpl.html',
+//         link: function(scope, elem, attrs){
+//             console.log(document.getElementById("entryPO").innerHTML);
+//             document.getElementById("entryPO").innerHTML = "{{ object[1] === '" + String(attrs.type) + "' ? object[0] : '' }}";
+//         }
+//     }
+// })  
