dc28fd82c7736ecf53d9a2b10425db9a2f710f44
Andrew Song <Andrew Song>
2016-06-30 11:03:32 -0700

    Change script source path

--- static/app/components/index/indexDIrs.js
+++ static/app/components/index/indexDIrs.js
@@ -1,6 +1,6 @@
 angular.module("myApp").directive("indexSearchRes", function () {
     return {
         replace: false,
-        templateUrl: 'templates/indexSearchResTmpl.html',
+        templateUrl: '/templates/indexSearchResTmpl.html',
     };
 });
