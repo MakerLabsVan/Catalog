db50fe2c6a711440205de55636dcbc8a0df6bbb6
Sunny <sunny.hl.leung@gmail.com>
2016-07-06 16:47:11 -0700

    remove resize code

--- static/app/components/d3/d3map.js
+++ static/app/components/d3/d3map.js
@@ -59,7 +59,6 @@ app.controller('mapController', ['$scope', '$window', '$location', function ($sc
     var height2 = $scope.map2.height();
     $scope.map1.studio.resize(width1, height1);
     $scope.map2.studio.resize(width2, height2);
-    $scope.map1.currentLocMarker.resize(width1, height1);
     // manuall $digest required as resize event
     // is outside of angular
     $scope.$digest();
