0cbc9878efdb7b5383d740b73267c8760beeb063
Andrew Song <Andrew Song>
2016-07-20 15:57:35 -0700

    Remove extra require calls in public api

--- static/app/components/googlesheets/public_gapi.js
+++ static/app/components/googlesheets/public_gapi.js
@@ -1,10 +1,7 @@
 var sheetKeyPrivate = "1MJpC2n-ekpnRXaLsb7B4dI6VOQIzn1eZO61I7sy2yiA";
-var sheetKeyPublic = "1trhKqK2g4hIEqnn0Acn4a8iqakYTpdmDF-POaCdZpX8";
 
 var google = require('googleapis');
-var googleAuth = require('google-auth-library');
 
-var SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
 var API_KEY = 'AIzaSyDIQ1Yb3d-VREZlm2WJ6DbtiLQgGA914F4';
 
 var public_getDataList = function (callback) {
@@ -21,5 +18,5 @@ var public_getDataList = function (callback) {
         var rows = response.values;
         callback(rows);
     });
-}
+};
 exports.public_getDataList = public_getDataList;
