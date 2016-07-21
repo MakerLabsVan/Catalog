86498c082ba1a048a256ac86f72e0c5c98d6924d
Andrew Song <Andrew Song>
2016-07-21 14:29:36 -0700

    Create routes.js file for route handlers

---
+++ routes.js
@@ -0,0 +1,36 @@
+module.exports = function (router, path, serverOps, public_serverOps) {
+
+    router.get("/", function (req, res) {
+        res.sendFile(path + '/static/views/index.html');
+        console.log()
+    });
+
+    router.get("/input", function (req, res) {
+        res.sendFile(path + '/static/views/input.html');
+    });
+
+    router.post("/new", function (req, res) {
+        serverOps.parse(req.body[0], req.body[1], function (response) {
+            return res.json(response);
+        });
+    });
+
+    router.post("/delete", function (req, res) {
+        serverOps.delEntry(req.body[0], function (result) {
+            return res.json(result);
+        });
+    });
+
+    router.get("/getCatalog", function (req, res) {
+        serverOps.getData(function (result) {
+            return res.json(result);
+        });
+    });
+
+    router.get("/publicGetCatalog", function (req, res) {
+        public_serverOps.public_getDataList(function (result) {
+            return res.json(result);
+        });
+    });
+
+};
--- static/app/components/googlesheets/oauth2login.js
+++ static/app/components/googlesheets/oauth2login.js
@@ -1,2 +1,19 @@
 var google = require('googleapis');
+// create spreadsheets client
+var sheets = google.sheets('v4');
+// oauth2 client
+var OAuth2 = google.auth.OAuth2;
+
+// client information
+var CLIENT_ID = '';
+var CLIENT_SECRET = '';
+var REDIRECT_URL = '';
+var SCOPES = 'https://www.googleapis.com/auth/spreadsheets';
+
+// instantiate OAuth2 client
+var oauth2Client = new OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
+var url = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES
+});
 
